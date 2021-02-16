eb8d5b3638a024ebf36147e40bd18112524e2fc1
ReeseKnapp <78838180+ReeseKnapp@users.noreply.github.com>
2021-02-16 11:47:12 -0600

    update

---
+++ src/driver.java
@@ -0,0 +1,11 @@
+public class driver
+{
+	
+	public static void main(String[] args)
+	{
+		MagictheGathering c1= new MagictheGathering("Grimgrim", 3, 5, 5);
+		MagictheGathering c2= new MagictheGathering("Gisa and Geralf", 2, 4, 4);
+		c1.display();
+		c2.display();
+	}
+}
